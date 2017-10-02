a8ca17ec6a29dd691ec8fced5280deff88de2b35
Suyash Katiyar <katiyar.suyash@gmail.com>
2017-10-02 16:13:51 +0530

    added support to android

--- android/app/src/main/java/com/lettertrack/MainApplication.java
+++ android/app/src/main/java/com/lettertrack/MainApplication.java
@@ -2,6 +2,7 @@ package com.lettertrack;
 
 import android.app.Application;
 
+import org.pgsqlite.SQLitePluginPackage;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SQLitePluginPackage(),
             new VectorIconsPackage()
       );
     }
